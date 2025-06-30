eabee3bfef611c7b259f745cb4476527a4eeb3a8
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2025-06-30 11:28:13 +0700

    Create bot.js

---
+++ src/bot.js
@@ -0,0 +1,40 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { readdirSync } = require('fs');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.commands = new Collection();
+client.cooldowns = new Collection();
+client.voiceStates = new Collection(); // Store voice connections and queues
+client.ffmpeg = require('ffmpeg-static');
+
+// Load commands
+const commandFolders = readdirSync('./src/commands');
+for (const folder of commandFolders) {
+    const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+}
+
+// Load events
+const eventFiles = readdirSync('./src/events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.login(process.env.DISCORD_TOKEN);
